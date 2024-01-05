6c523b7240b629e901220cf1136b0e30ff9bf274
Mitesh singh <102028317+Mitesh957@users.noreply.github.com>
2024-01-05 15:25:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ sign_up_btn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
 });
 
+
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
